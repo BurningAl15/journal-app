0a70c3912122f2265c1412afc890b823f8762152
Aldhair Vera <aldhairvera15@gmail.com>
2023-08-22 14:52:46 -0500

    Add ImageGallery - Add button in JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../../views";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
@@ -14,10 +15,24 @@ export const JournalPage = () => {
       </Typography> */}
 
       {/* Nothing selected */}
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
       {/* Note View  */}
-      <NoteView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
